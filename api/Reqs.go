59eaedc83754fe613f0195f7b5864951db88fbc3
Drack112 <onepessoa3@gmail.com>
2022-12-13 14:07:40 -0300

    🐈 Make the HTML data of request be saved on HTML File located on 'data' folder

--- api/Reqs.go
+++ api/Reqs.go
@@ -2,6 +2,8 @@ package api
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/leoff00/gocheckitout/helpers"
 )
@@ -30,8 +32,21 @@ func MakeRequestWithDetails(url string) error {
 	}
 
 	if res.StatusCode == 200 {
-		fmt.Printf("Site loaded %s \n your status is %d \n timestamp: %s \n body: %s", res.Url, res.StatusCode, res.Timestamp, string(*res.Body))
-	}
+		//fmt.Printf("Site loaded %s \n your status is %d \n timestamp: %s \n body: %s", res.Url, res.StatusCode, res.Timestamp, string(*res.Body))
 
+		file := fmt.Sprintf("data_%s.html", string(res.Url.Host))
+
+		f, err := os.Create("./data/" + file)
+		if err != nil {
+			log.Panicf("Error in creating the file of %s\n Error: %s", f.Name(), err)
+		}
+
+		defer f.Close()
+
+		f.WriteString(string(*res.Body))
+
+		fmt.Printf("Site loaded %s \n your status is %d \n timestamp: %s \n file: %s \n", res.Url, res.StatusCode, res.Timestamp, f.Name())
+
+	}
 	return nil
 }
