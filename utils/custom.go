866cbef5c994a2c9ff4eb20e9c7c42ad621edd3a
leoff00 <leof0180@gmail.com>
2022-12-13 11:06:23 -0300

    docs: remaking the gocheckitout docs and changelog

--- utils/custom.go
+++ utils/custom.go
@@ -9,10 +9,10 @@ import (
 // Customize the response with the way that you want
 // But remember that respose must be legit in according within http struct.
 type Custom struct {
-	Header     http.Header
-	StatusCode int16
-	RawBody    *io.ReadCloser
-	Body       *[]byte
-	Url        *url.URL
-	Timestamp  string
+	Header     http.Header    // Header contents like content-type, accept, etc...
+	StatusCode int16          //output default status code - 200. (This app only returns 200 if OK, if don't will return the specified configurated status code from errors.)
+	RawBody    *io.ReadCloser //The raw body not converted in slice of byte, giving an options to optimize the request body as you want
+	Body       *[]byte        // The body converted in slice of bytes.
+	Url        *url.URL       //the own url of the request that you are specifying in the dummy text file.
+	Timestamp  string         //Default timestamp (IN BRAZILIAN TIMEZONE) of the moment of request.
 }
